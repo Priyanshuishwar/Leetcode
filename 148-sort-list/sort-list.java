0acc107c0f5e4bd5262e5cce0e032c33bf0eb079
Priyanshu Kumar <140679451+Priyanshuishwar@users.noreply.github.com>
2025-05-15 17:42:00 +0530

    Time: 9 ms (96.05%) | Memory: 57.1 MB (39.14%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -10,18 +10,47 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
-        ListNode temp=head;
-        while(temp!=null){
-            pq.add(temp.val);
-            temp=temp.next;
+        if(head == null || head.next == null){
+            return head;
         }
-        temp = head;
-        while(!pq.isEmpty() && temp!=null){
-            temp.val = pq.poll();
-            temp=temp.next;
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return head;
+        ListNode mid = slow.next;
+        slow.next = null;
+
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+
+        return merge(left,right);
 
     }
+    public static ListNode merge(ListNode l1,ListNode l2){
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+
+        while(l1 != null && l2 != null){
+            if(l1.val > l2.val){
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            else{
+                curr.next = l1;
+                l1 = l1.next;
+            }
+            curr = curr.next;
+        }
+        if(l1 != null){
+            curr.next = l1;
+        }
+        else{
+            curr.next = l2;
+        }
+        // curr.next = l2;
+        return dummy.next;
+    } 
 }
