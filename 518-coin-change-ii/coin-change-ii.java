801daf10efc3d81eddc495aca080f0c6ddc8c338
Priyanshu Kumar <140679451+Priyanshuishwar@users.noreply.github.com>
2025-06-11 22:35:53 +0530

    Time: 10 ms (50.97%) | Memory: 49 MB (49.54%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,30 +1,21 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int dp[][] = new int[301][5001];
-        for(int p[] : dp){
-            Arrays.fill(p,-1);
-        }
+        int dp[][] = new int[n + 1][amount + 1];
 
-        return helper(coins,n,amount,0,dp);
-    }
-    public static int helper(int coin[],int n,int amount,int i,int dp[][]){
-        if(amount == 0){
-            return 1;
+        for(int i = 0;i<=n;i++){
+            dp[i][0] = 1;
         }
-        if(i == n){
-            return 0;
+        for(int i =1;i<= n;i++){
+            for(int j=1;j<=amount;j++){
+                if(coins[i-1] <= j){
+                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
         }
-        if(coin[i] > amount){
-            return dp[i][amount] = helper(coin,n,amount,i+1,dp);
-        }
-        if(dp[i][amount] != -1){
-            return dp[i][amount];
-        }
-        int take = helper(coin,n,amount - coin[i],i,dp);
-        int skip = helper(coin,n,amount,i + 1,dp);
-
-        dp[i][amount] = take + skip;
-        return dp[i][amount];
+        return dp[n][amount];
     }
 }
