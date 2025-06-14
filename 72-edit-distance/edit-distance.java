78099e733d7401488d774b91e26e12f263e45d5d
Priyanshu Kumar <140679451+Priyanshuishwar@users.noreply.github.com>
2025-06-14 19:27:02 +0530

    Time: 5 ms (61.69%) | Memory: 45.1 MB (62.72%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -2,33 +2,24 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
+
         int dp[][] = new int[n + 1][m + 1];
-
-        for(int p[] : dp){
-            Arrays.fill(p,-1);
+        for(int i = 0;i <= n;i++){
+            dp[i][0] = i;
         }
-
-        return helper(word1,word2,n-1,m-1,dp);
-    }
-    public static int helper(String s1,String s2,int i,int j,int dp[][]){
-        if(i < 0){
-            return j + 1;
+        for(int j = 0;j <= m;j++){
+            dp[0][j] = j;
         }
-        if(j < 0){
-            return i + 1;
+        for(int i = 1; i <= n;i++){
+            for(int j = 1; j <= m;j++){
+                if(word1.charAt(i - 1) == word2.charAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else{
+                    dp[i][j] =1 + Math.min(dp[i - 1][j],Math.min(dp[i][j -1],dp[i - 1][j - 1]));
+                }
+            }
         }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        if(s1.charAt(i) == s2.charAt(j)){
-            return helper(s1,s2,i-1,j-1,dp);
-        }
-        else{
-            int insert = 1 + helper(s1,s2,i,j-1,dp);
-            int delete = 1 + helper(s1,s2,i-1,j,dp);
-            int replace = 1 + helper(s1,s2,i-1,j-1,dp);
-
-            return dp[i][j] = Math.min(insert,Math.min(delete,replace));
-        }
+        return dp[n][m];
     }
 }
