f028f40a12095b553739f708c2e504796215b096
Priyanshu Kumar <140679451+Priyanshuishwar@users.noreply.github.com>
2025-06-14 08:43:07 +0530

    Time: 26 ms (56.43%) | Memory: 45.9 MB (17.03%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -4,27 +4,18 @@ class Solution {
         StringBuilder sb = new StringBuilder(s);
         sb.reverse();
         String s1 = sb.toString();
+
         int dp[][] = new int[n + 1][n + 1];
-        for(int p[] : dp){
-            Arrays.fill(p,-1);
+        for(int i = 1;i <= n;i++){
+            for(int j= 1;j <= n;j++){
+                if(s.charAt(i-1) == s1.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j - 1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j -1]);
+                }
+            }
         }
-
-        int ans = helper(s,s1,n,0,0,dp);
-        return n - ans;
-
-    }
-    public static int helper(String s,String s1,int n,int i,int j,int dp[][]){
-        if(i == n || j == n){
-            return 0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        if(s.charAt(i) == s1.charAt(j)){
-            return dp[i][j] = 1 + helper(s,s1,n,i + 1,j + 1,dp);
-        }
-        else{
-            return dp[i][j] = Math.max(helper(s,s1,n,i + 1,j,dp),helper(s,s1,n,i,j + 1,dp));
-        }
+        return  n - dp[n][n];
     }
 }
