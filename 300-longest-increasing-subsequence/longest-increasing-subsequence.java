9d5d252d3886cc28174a0d7e24f52ed5f66e1dbd
Priyanshu Kumar <140679451+Priyanshuishwar@users.noreply.github.com>
2025-06-16 15:27:08 +0530

    Time: 39 ms (64.04%) | Memory: 44.4 MB (56.11%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,24 +1,17 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int dp[][] = new int[n + 1][n + 1];
-        for(int p[] : dp){
-            Arrays.fill(p,-1);
+        int n=nums.length;
+        int dp[] = new int[n];
+        Arrays.fill(dp,1);
+        int ans=1;
+        for(int i=0;i<n;i++){
+            for(int j=i-1;j>=0;j--){
+                if(nums[i]>nums[j]){
+                    dp[i]= Math.max(dp[i],dp[j]+1);
+                }
+            }
+            ans =Math.max(dp[i],ans);
         }
-
-        return helper(nums,0,-1,n,dp);
-    }
-    public static int helper(int nums[],int indx,int prevvIndx,int n,int dp[][]){
-        if(indx == n){
-            return 0;
-        }
-        if(dp[indx][prevvIndx + 1] != -1){
-            return dp[indx][prevvIndx + 1];
-        }
-        int len = 0 + helper(nums,indx + 1,prevvIndx,n,dp);
-        if(prevvIndx == -1 || nums[indx] > nums[prevvIndx]){
-            len = Math.max(len,1 + helper(nums,indx + 1,indx,n,dp));
-        }
-        return dp[indx][prevvIndx + 1] =  len;
+        return ans;
     }
 }
