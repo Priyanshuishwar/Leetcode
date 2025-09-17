99cd10f9bdaee9d91cd64a99858e044ad2116d20
Priyanshu Kumar <140679451+Priyanshuishwar@users.noreply.github.com>
2025-09-17 14:53:12 +0530

    Time: 4 ms (59.40%) | Memory: 48.1 MB (8.04%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,25 +1,25 @@
 class Solution {
+    // Function to return the minimum cost to react at bottom
+    // right cell from top left cell.
     public int minPathSum(int[][] grid) {
+        // Code here
         int n = grid.length;
         int m = grid[0].length;
+        
         int dp[][] = new int[n][m];
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i == 0 && j == 0){
-                    dp[i][j] = grid[i][j];
-                }
-                else if(i == 0){
-                    dp[i][j] = grid[i][j] + dp[i][j-1]; 
-                }
-                else if(j == 0){
-                    dp[i][j] = grid[i][j] + dp[i-1][j];
-                }
-                else{
-                    dp[i][j] = Math.min(grid[i][j] + dp[i-1][j] , dp[i][j-1] + grid[i][j]);
-                }
+        dp[0][0] = grid[0][0];
+        for(int i = 1; i < m; i++){
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        for(int i = 1; i < n; i++){
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
             }
         }
-        return dp[n-1][m-1];
+        return dp[n - 1][m - 1];
     }
 }
